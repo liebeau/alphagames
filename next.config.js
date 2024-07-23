0b9962f4bf0847f64988ff75645ee1c628c6798e
liebeau <70014197+liebeau@users.noreply.github.com>
2024-07-23 20:16:25 +0800

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+module.exports = {
+   
+  }
+
+
+const nextConfig = {
+    images: {
+        domains: ['rational.oss-cn-shenzhen.aliyuncs.com']
+      }
+
+}
 
 module.exports = nextConfig
