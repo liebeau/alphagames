f835e865140f2966dcf604294f73ea73cf9a99dc
liebeau <70014197+liebeau@users.noreply.github.com>
2025-03-26 05:27:02 +0800

    Update footer.tsx

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -5,9 +5,11 @@ const Footer = () => {
     <footer className="bg-neutral-50 border-t border-neutral-200">
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
+        <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
           <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
             后宫八千·齐人之福
           </h3>
+          </div>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
               href="https://dy.liujiehui.com/s/jinshan.php?tkey=2892173faf"
