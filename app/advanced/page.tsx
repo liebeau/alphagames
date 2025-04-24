d8eba540ac1ffe9cef65d6430f1cd3d42296ba11
liebeau <70014197+liebeau@users.noreply.github.com>
2025-04-24 20:09:43 +0800

    Add files via upload

---
+++ app/advanced/page.tsx
@@ -0,0 +1,17 @@
+import Footer from "../components/footer"
+import Header from "../components/header"
+import Advanced from "../components/advanced"
+
+
+export default function Page() {
+    return (
+    <>
+    <Header/>
+    <br/>
+    <Advanced/>
+    <br/>
+    <Footer/>
+
+    </>
+    )
+  }
