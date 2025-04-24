27a15ab08d0dcbceacbed2eee6cd775b29b271f1
liebeau <70014197+liebeau@users.noreply.github.com>
2025-04-24 17:55:48 +0800

    Update basic.tsx

--- app/components/basic.tsx
+++ app/components/basic.tsx
@@ -22,17 +22,6 @@ const Basic = () => {
         <Image
       width={800}
       alt="Basic Image"
-      src="https://rational.oss-cn-shenzhen.aliyuncs.com/Male/799.png"
-    />
-      </div>
-      <div style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center'
-      }}>
-        <Image
-      width={800}
-      alt="Basic Image"
       src="https://rational.oss-cn-shenzhen.aliyuncs.com/Male/shit.png"
     />
       </div>
@@ -43,123 +32,9 @@ const Basic = () => {
       <br />
       <div className="flex flex-col items-center justify-center">
         <div className="max-w-2xl w-full bg-white shadow-md rounded-lg p-6">
-          <h1 className="text-3xl font-bold mb-4">什么是红药丸？</h1>
+          <h1 className="text-3xl font-bold mb-4">升级录制中</h1>
           <p className="text-red-500 italic font-bold text-lg mb-2">
-                 正统的红药丸主张：男女是最好的互补。
-          </p>
-          <p className="text-gray-700 text-lg mb-2">
-                 红药丸是红丸教父Rollo Tomassi借用了电影黑客帝国里面的桥段，帮助主角Neo脱离母体环境的解药叫红药丸，与之相对的就是继续留在虚幻世界醉生梦死的蓝药丸，在2000年初教父开始在论坛写文章，并且当时还有很多把妹达人先驱不断的去研究两性真实动态的结果，才有这些红药丸的知识，到现在已经有二十多年了。
-          </p>
-          <p className="text-gray-700 text-lg mb-2">
-                 严格来说红药丸并不是教你怎么把妹的。因为它只提供了理论架构和整体大方向，用军事术语来说，红药丸只提供战略，没有提供相关战术。
-          </p>
-          <p className="text-gray-700 text-lg mb-2">
-                 红药丸是理论，PUA是实践。举个例子：你用的泡妞技术是实实在在作用在妹子身上，她给你丢废测，你就用技术破解废测，这比较像踩在地上的进行两军交战；而红药丸就像是在天上俯视地下，站在上帝视角观察和指导你和妹子之间的关系框架平衡与走向，你是往舔狗、马子狗Beta方向走，还是能主导关系让妹子追随你。
-          </p>
-          <p className="text-gray-700 text-lg">
-                 普及一个基础知识，你们在网络上看到的红药丸、蓝药丸、紫药丸、米格道、MRA、黑药丸等等，这些都是一个群体、族群，之所以名称不一样，是因为他们的主张、解决方案不一样。例如：红药丸倡导男女是最好的互补，而米格道是主张不要和女人进入长期关系。
-          </p>
-        </div>
-      </div>
-      <br />
-      <div className="flex flex-col items-center justify-center">
-        <div className="max-w-2xl w-full bg-white shadow-md rounded-lg p-6">
-          <h1 className="text-3xl font-bold mb-4">极速版</h1>
-          <p className="text-black-888 font-bold text-lg mb-2">
-                 课程目标：短期快速拿到结果，快速上手、快速约会、快速收尾
-          </p>
-          <p className="text-gray-700 text-lg mb-2">
-                 学与两性相关的知识都逃不出红药丸、Outer Game和Inner Game这三个范畴，Game是实践，红药丸是理论。Game主要是教你一些能够挑逗女人情绪、激起她们欲望的实操技巧，从而让你顺利的打到炮解决你性匮乏的问题，至于妹子的质量如何、你们的关系是否稳定幸福，这些都是教PUA、教Game的人不管的。
-          </p>
-          <p className="text-gray-700 text-lg mb-2">
-                 然而在学Game前先学红药丸的知识，那么你就有了关系风险、欲望动态、框架动态平衡、关系维护等等在关系相处和关系中所需要的认知，这就是所谓的预防胜于治疗。这一整套课程就是针对这三个模块展开制作的，目的是让新手快速入门、快速拿到结果。
-          </p>
-          <p className="text-gray-700 text-lg">
-                 引用《管子·牧民》中的一句话：仓廪实而知礼节，衣食足而知荣辱；对于长期单身、长时间没有性生活的男人来说，拥有一段良好的恋爱关系还是其次，这个时代大部分男人就像是好几十年没吃饭的饿死鬼，当下重中之重是先让你们吃饱（拿到结果、打到炮），只有让你吃饱后才能沉下心来去思考和选择适合发展长期关系的伴侣。
-          </p>
-          <Image
-          width={1024}
-          alt="SMV Image"
-          src="https://rational.oss-cn-shenzhen.aliyuncs.com/Male/Male.png"
-        />
-          <p className="text-gray-700 text-lg">
-                 和女人从0到1发展一段关系主要是经历上面的一整个流程，极速版主要侧重于网聊、约会技术实操层面以及一些关系维护和心态层面的内容，如果你和女人约会时不知道说什么做什么、不知道怎么升级推进，这套课能快速解决你的问题，如果你是那种自身建设还不错的学员，学完当晚约会，很大概率会有机会收尾。
-          </p>
-          <p className="text-red-500 font-bold text-lg italic">
-                 一个字：快！
-          </p>
-        </div>
-      </div>
-      <br />
-      <div className="flex flex-col items-center justify-center">
-        <div className="max-w-2xl w-full bg-white shadow-md rounded-lg p-6">
-          <h1 className="text-3xl font-bold mb-4">完整版</h1>
-          <p className="text-black-888 font-bold text-lg mb-2">
-                 完整版在极速版的基础上加了更多内容、更多技术，更重要的是告诉你这些技术的底层逻辑、技术为什么起效果，从而树立一个知识架构，不至于走到泡学中毒的地步。
-          </p>
-          <p className="text-gray-700 text-lg mb-2">
-                 问：只背招式、学技术，不知道技术的底层逻辑会怎样？
-          </p>
-          <p className="text-gray-700 text-lg mb-2">
-                 答：PUA1.0技术流的祖师爷迷男集万千技术于一身、斩女无数，在追到他的真命天女Katya后，他所有的PUA技术通通失灵，最后还吸毒把自己搞进了精神病院。
-          </p>
-          <p className="text-gray-700 text-lg">
-                 这就是因为他不知道这些技巧为什么会起效果，他就是在背招式，知其然不知其所以然。只学泡妞技术不懂红丸，短期能让你取得结果打到炮，把你和女人相处的时间维度拉长，终究还是短多长空，暴雷是迟早的事。
-          </p>
-          <p className="text-gray-700 text-lg">
-                 新手小白用惯例、背招式，中高级选手用方法。这套课程针对两性交往的全流程，给出了大量的惯例话术和方法，如果你是新手小白，就先靠记惯例话术快速上手拿结果，其中有特别为你整理出30条女人常问的废测惯例为你披荆斩棘、还有在私密空间如何顺利打到炮等等。
-          </p>
-          <p className="text-gray-700 text-lg">
-                 如果你已经有了一些Game的经验，那你就会用我所教的方法创造出无限属于你个人的惯例话术，不需要死记硬背，而是懂得现场根据女人的反应做微调，真正把这些技术内化。
-          </p>
-          <br/>
-          <p className="text-black-500 font-bold text-lg italic">
-                 The End Is Not The End！（课程持续更新中&往后会有私密直播课。。。。。。）
-          </p>
-        </div>
-      </div>
-      <br />
-      <div className="flex flex-col items-center justify-center">
-        <div className="max-w-2xl w-full bg-white shadow-md rounded-lg p-6">
-          <h1 className="text-3xl font-bold mb-4">如果你：</h1>
-          <p className="text-black-888 text-lg mb-2 font-bold italic">
-          1.关系中被女人牵着鼻子走、女人对你没欲望、把你当哈巴狗
-          </p>
-          <p className="text-black-888 text-lg mb-2 font-bold italic">
-          2.想要学习红药丸、红丸觉醒、成为Alpha完全体
-          </p>
-          <p className="text-black-888 text-lg mb-2 font-bold italic">
-          3.不知道如何主导关系、不被女人驯化操控
-          </p>
-          <p className="text-black-888 text-lg mb-2 font-bold italic">
-          4.需求感强、内心柔弱容易被撼动
-          </p>
-          <p className="text-black-888 text-lg mb-2 font-bold italic">
-          5.不知道怎么和女人聊天
-          </p>
-          <p className="text-black-888 text-lg mb-2 font-bold italic">
-          6.不知道怎么破解女人的废物测试
-          </p>
-          <p className="text-black-888 text-lg mb-2 font-bold italic">
-          7.不知道怎么推进关系
-          </p>
-          <p className="text-black-888 text-lg mb-2 font-bold italic">
-          8.不知道怎么让女人爱上你
-          </p>
-          <p className="text-black-888 text-lg mb-2 font-bold italic">
-          9.不知道约会说什么、做什么、怎么推进升级
-          </p>
-          <p className="text-black-888 text-lg mb-2 font-bold italic">
-          10.不知道私密空间怎么顺利收尾
-          </p>
-          <p className="text-black-888 text-lg mb-2 font-bold italic">
-          等等等等。。。。。。
-          </p>
-          <p className="text-red-500 text-lg mb-2 font-bold italic">
-                 这一系列问题以及更多的疑难杂症，都将会在这套课程里得到答案。
-          </p>
-          <p className="text-black-888 text-lg mb-2 font-bold italic bg-yellow-300 p-2 inline">
-          限时优惠极速版仅需￥398、完整版￥799，赶紧找我下单（WeChat ID：alpha91game），早日兑现你的情感自由！
+                 敬请期待
           </p>
         </div>
       </div>
