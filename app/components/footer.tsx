03b037cc1b13b331bd1de81719dd81d5ea5df641
liebeau <70014197+liebeau@users.noreply.github.com>
2025-04-24 18:00:05 +0800

    Update footer.tsx

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -12,7 +12,7 @@ const Footer = () => {
           </div>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
-              href="https://dy.liujiehui.com/s/jinshan.php?tkey=2892173faf"
+              href="./contact"
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
               target='_blank'
             >
